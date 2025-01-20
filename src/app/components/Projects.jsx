f4cfef5417703be07f1b8a7fbe296f272493f1a5
Gina González <gini_go@hotmail.com>
2025-01-20 18:28:03 +0100

    add new styles for projecs compononets

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import setProject from "../../../public/setProject.png";
-import spotsBarcelona from "../../../public/spotsbarcelona2.png";
+import spotsBarcelona from "../../../public/spotsbarcelona.png";
 import "../styles/components/Projects.css";
 import taskMaster from "../../../public/taskMaster.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,7 +10,9 @@ function Projects() {
   return (
     <div className="projects">
       <div className="projects__intro">
-        <p className="projects__tittle">Recent Projects</p>
+        <h2 className="text-4xl">
+          Recent <span className="projects__fulltittle">Projects</span>
+        </h2>
         <button className="bg-brand rounded-full w-40 py-2 text-white	">
           View All Projects <FontAwesomeIcon icon={faArrowRight} />
         </button>
@@ -18,17 +20,20 @@ function Projects() {
 
       <div className="projects__boxes">
         <div className="project">
-          <Image
-            className="project__image"
-            src={setProject}
-            alt="photo of Project"
-          />
-        </div>
-        <div className="project">
           <Image className="project__image" src={spotsBarcelona} alt="" />
+          <p className="project__title">Task List Application</p>
+          <p className="project__text">
+            Online tool to manage and organize your daily tasks.
+            <br /> Stack: React - Vite - SASS - React Router
+          </p>
         </div>
         <div className="project">
           <Image className="project__image" src={taskMaster} alt="" />
+          <p className="project__title"> Best Spots in Barcelona</p>
+          <p className="project__text">
+            A web guide to discover the best places in Barcelona. <br />
+            Stack: React - Vite - SASS - React Router - MySQL - Express
+          </p>
         </div>
       </div>
     </div>
