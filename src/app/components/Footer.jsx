7dc287a4fcbc9d38929f381a9f9766ac005cf59a
Gina González <gini_go@hotmail.com>
2025-01-23 10:35:59 +0100

    add styles to footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,27 +3,39 @@ import Menu from "./Menu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import "../styles/components/Footer.css";
 
 function Footer() {
   return (
-    <div>
-      <div>Logo</div>
-      <Menu />
-      <div>
-        <nav>
-          <a href="https://www.linkedin.com/in/ginagonzalezgut/">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-          <a href="https://github.com/Ginagonzalezgut">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a href="mailto:ginagonzalezgut@gmail.com">
-            <FontAwesomeIcon icon={faEnvelope} />
-          </a>
-        </nav>
-        <p>Copyright © 2025 Gina González Gutiérrez</p>
+    <footer className="footer ">
+      <div className="container">
+        <div className="row">
+          <div className="col">Logo</div>
+          <div className="col">
+            {" "}
+            <Menu />
+          </div>
+
+          <div className="col">
+            <nav>
+              <a href="https://www.linkedin.com/in/ginagonzalezgut/">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+              <a href="https://github.com/Ginagonzalezgut">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a href="mailto:ginagonzalezgut@gmail.com">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+            </nav>
+          </div>
+        </div>
+        <div className="row">
+          {" "}
+          <p>Copyright © 2025 Gina González Gutiérrez</p>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
