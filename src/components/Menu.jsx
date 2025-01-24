488acc41e4636d2b18fa899243d3d57d17917904
Gina González <gini_go@hotmail.com>
2025-01-24 17:13:24 +0100

    Use Next Link

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "../styles/components/Menu.css";
+import Link from "next/link";
 
 function Menu() {
   return (
     <nav className="menu">
+      <Link href="/" />
       <a href="/">Home</a>
       <a href="/projects">Projects</a>
       <a href="/about">About</a>
-      <a href="/#contact">Contact</a>
+      <Link href="/#contact">Contact</Link>
     </nav>
   );
 }
