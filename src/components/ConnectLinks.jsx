2e736d852053b93ceae88c50743eff8b702b3710
Gina González <gini_go@hotmail.com>
2025-01-26 13:27:58 +0100

    adjust styles for mobile

--- src/components/ConnectLinks.jsx
+++ src/components/ConnectLinks.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 function ConnectLinks() {
   return (
     <div className="container">
-      <div className="row  g-3 g-md-0">
-        <div className="col-12 col-md">
+      <div className="row">
+        <div className="col">
           <a
             className="portfolio__connect__link"
             href="https://github.com/Ginagonzalezgut"
             target="_blank"
           >
-            <FontAwesomeIcon icon={faGithub} className="fa-fw" /> Github
+            <FontAwesomeIcon icon={faGithub} className="fa-fw" />{" "}
+            <span className="portfolio__name-links">Github</span>
           </a>
         </div>
 
-        <div className="col-12 col-md">
+        <div className="col">
           <a
             className="portfolio__connect__link"
             href="https://www.linkedin.com/in/ginagonzalezgut/"
             target="_blank"
           >
-            <FontAwesomeIcon icon={faLinkedin} className="fa-fw" /> Linkedin
+            <FontAwesomeIcon icon={faLinkedinIn} className="fa-fw" />
+            <span className="portfolio__name-links"> Linkedin</span>
           </a>
         </div>
       </div>
