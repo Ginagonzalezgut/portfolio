3df12d8368d490760a38ddaa278cea0e51306994
Gina González <gini_go@hotmail.com>
2025-01-28 15:59:33 +0100

    Styles of Contact

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -16,22 +16,26 @@ function About() {
 
       <div className="about__info">
         <div className="container">
-          <div className="d-flex">
-            <Image className="about__photo" src={gina} alt="" unoptimized />
-            <div className="about__desc">
-              <Heading title="The story behind Gina" />
-              <p>
-                One day, I decided to transition from teaching to creating and
-                discovered my passion for web development. I love building
-                visually appealing and functional products, always paying close
-                attention to detail. I enjoy collaborating with others and
-                constantly learning.{" "}
-              </p>
-              <p>
-                When I'm not coding, I find inspiration by exploring specialty
-                coffee shops in my city. Want to know more about me or
-                collaborate on a project? Feel free to reach out!
-              </p>
+          <div className="row">
+            <div className="col-md-3">
+              <Image className="about__photo" src={gina} alt="" unoptimized />
+            </div>
+            <div className="col-md-9">
+              <div className="about__desc">
+                <Heading title="The story behind Gina" />
+                <p>
+                  One day, I decided to transition from teaching to creating and
+                  discovered my passion for web development. I love building
+                  visually appealing and functional products, always paying
+                  close attention to detail. I enjoy collaborating with others
+                  and constantly learning.{" "}
+                </p>
+                <p>
+                  When I'm not coding, I find inspiration by exploring specialty
+                  coffee shops in my city. Want to know more about me or
+                  collaborate on a project? Feel free to reach out!
+                </p>
+              </div>
             </div>
           </div>
         </div>
