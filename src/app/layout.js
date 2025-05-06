d7bd8f2010158305768d6299b9a02543578327e0
Gina González <gini_go@hotmail.com>
2025-05-06 10:48:34 +0200

    Add meta imnfo

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,41 @@ import Footer from "@/components/Footer";
 config.autoAddCss = false;
 
 export const metadata = {
-  title: "Gina González",
-  description: "Soy Gina González, Front end developer",
+  title: "Gina González Gut | Frontend Developer",
+  description:
+    "I'm Gina, a frontend developer. I create clean and functional UX/UI interfaces with well-structured code and attention to detail.",
+  alternates: {
+    canonical: "https://ginagonzalezgut.com",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  openGraph: {
+    title: "Gina González Gut | Frontend Developer",
+    description:
+      "I'm Gina, a frontend developer. I create clean and functional UX/UI interfaces with well-structured code and attention to detail.",
+    url: "https://ginagonzalezgut.com",
+    siteName: "Gina González Gut",
+    locale: "en_US",
+    images: [
+      {
+        url: "https://ginagonzalezgut.com/website-image.jpg",
+        width: 600,
+        height: 350,
+        alt: "Gina González Website",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
